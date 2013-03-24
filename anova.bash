ed09d2ad9b939ad6d651015d293d50169d5d4b55
Estevo U. C. Castro <euccastro@gmail.com>
2013-03-24 10:57:00 +0100

    Anovo miga-salt, comentários nos scripts de conveniência.

--- anova.bash
+++ anova.bash
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Anova a configuraçom do servidor com respeito à do repo miga-salt.
+#
+# Hai que usá-lo com root ou com um utente que tenha sudo sem contrasinal.
+#
+# Exemplo: ./anova.bash es@mancomunidadeintegralgalega.net
 
 if [ -z $1 ]
     then
--- infecta.bash
+++ infecta.bash
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-# Inicializa o servidor na IP que lhe damos
+# Inicializa o servidor na IP que lhe damos para que funcione como um
+# amo/escravo de Salt coa configuraçom no repositório miga-salt.
+#
+# Hai que usá-lo co utente de ssh root ou que tenha sudo sem contrasinal.
+#
+# Para nom ter que andar tecleando o IP, e enquanto nom termos um domínio
+# apontando a essa máquina, é recomendado ter um alias para ela em
+# /etc/hosts .
+#
+# Exemplo: ./infecta.bash root@miga
 
 if [ -z $1 ]
     then
